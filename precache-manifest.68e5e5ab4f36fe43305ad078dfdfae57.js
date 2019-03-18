98dd44dc2a8c257b9303181062fb7bd182ef99ee
Nelias <michal.antropik@gmail.com>
2019-03-18 13:56:35 +0100

    Updates

---
+++ precache-manifest.68e5e5ab4f36fe43305ad078dfdfae57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ee6eba955643df21423",
+    "url": "/photonic-photo-editor/static/css/main.d978a9fb.chunk.css"
+  },
+  {
+    "revision": "4ee6eba955643df21423",
+    "url": "/photonic-photo-editor/static/js/main.0332877e.chunk.js"
+  },
+  {
+    "revision": "1dd11362425cd581239c",
+    "url": "/photonic-photo-editor/static/js/runtime~main.5010435c.js"
+  },
+  {
+    "revision": "73080c8c5313bf1fb476",
+    "url": "/photonic-photo-editor/static/js/2.d519b009.chunk.js"
+  },
+  {
+    "revision": "aa083e9484f3e24821b8763cc82dbd98",
+    "url": "/photonic-photo-editor/index.html"
+  }
+];
